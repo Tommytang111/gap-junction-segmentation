7e288a048780f6859430025a73907b564a2d4c26
Zenan Tang <tommy111@narval2.narval.calcul.quebec>
2025-07-30 17:57:59 -0400

    Update double convolution block in Unet model

--- src/models.py
+++ src/models.py
@@ -202,6 +202,27 @@ class DoubleConv(nn.Module):
             mid_channels = out_channels
             
         self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False) if not three else nn.Conv3d(in_channels, mid_channels, kernel_size=(1,3,3), padding=1, bias=False),
+            nn.BatchNorm2d(mid_channels) if not three else nn.BatchNorm3d(mid_channels),
+            nn.ReLU(inplace=False),
+            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False) if not three else nn.Conv3d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels) if not three else nn.BatchNorm3d(out_channels),
+            nn.ReLU(inplace=False),
+            nn.Dropout(p=dropout)
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
+    
+class DoubleConv2(nn.Module):
+    """Second version of double convolution block used in UNet. Needs to be edited. Maybe TripleConv?"""
+    
+    def __init__(self, in_channels, out_channels, mid_channels=None, three=False, dropout=0):
+        super().__init__()
+        if not mid_channels:
+            mid_channels = out_channels
+            
+        self.double_conv = nn.Sequential(
             nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False) if not three else nn.Conv3d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(mid_channels) if not three else nn.BatchNorm3d(mid_channels),
             nn.ReLU(inplace=False),
